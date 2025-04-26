07fd44109fceb49287da21634242c0f6dc215b5e
S.M.Fahad Farabee <124309861+Fahad-Farabee@users.noreply.github.com>
2025-04-26 22:44:57 +0600

    Add files via upload

---
+++ user login system/includes/login_model.inc.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types=1);
+function get_user(object $pdo,string $username){
+    $quey = "SELECT * FROM users WHERE username = :username;";
+    $stmt = $pdo->prepare($quey);
+    $stmt->bindParam(":username", $username);
+    $stmt->execute();
+
+    $result = $stmt->fetch(PDO::FETCH_ASSOC);
+    return $result;
+}
